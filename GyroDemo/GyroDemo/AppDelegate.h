d933e49ce9a2b3e016021e17c1d064aee49c5478
ManIkWeet <r.v.d.p@live.nl>
2013-09-28 17:51:58 +0200

    Initial commit.

---
+++ GyroDemo/GyroDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GyroDemo
+//
+//  Created by ManIkWeet on 28-09-13.
+//  Copyright (c) 2013 RoKan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GyroDemo/GyroDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GyroDemo
+//
+//  Created by ManIkWeet on 28-09-13.
+//  Copyright (c) 2013 RoKan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
